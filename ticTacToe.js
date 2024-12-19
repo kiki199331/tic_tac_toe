f05be53bfd5e93106f5dc2f9b6114b3d5f016c3c
Cloris <cloris.w@inwinstack.com>
2024-12-20 01:44:02 +0800

    fix: remove unused code

--- ticTacToe.js
+++ ticTacToe.js
@@ -97,15 +97,6 @@ const checkWin = (lastMoveIndex) => {
         pattern.every(index => board[index] === currentPlayer)
     );
 
-    // const winPatterns = [
-    //     [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-    //     [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-    //     [0, 4, 8], [2, 4, 6] // diagonals
-    // ];
-    // return winPatterns.some(pattern =>
-    //     pattern.every(index => board[index] === currentPlayer)
-    // );
-
 };
 
 
